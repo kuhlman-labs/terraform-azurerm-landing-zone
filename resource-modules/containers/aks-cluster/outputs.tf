c546866c088f6ad2306c25721f303d773a11a7fa
Brett Kuhlman <brettkuhlman@Brett-Kuhlmans-MacBook-Pro.local>
2019-07-18 15:39:41 -0400

    test

--- resource-modules/containers/aks-cluster/outputs.tf
+++ resource-modules/containers/aks-cluster/outputs.tf
@@ -15,25 +15,25 @@ output "aks_kube_admin_config_raw" {
 }
 
 output "aks_kube_admin_config_client_key" {
-  value = azurerm_kubernetes_cluster.base.kube_admin_config.client_key
+  value = azurerm_kubernetes_cluster.base.kube_admin_config.*.client_key
 }
 
 output "aks_kube_admin_config_client_certificate" {
-  value = azurerm_kubernetes_cluster.base.kube_admin_config.client_certificate
+  value = azurerm_kubernetes_cluster.base.kube_admin_config.*.client_certificate
 }
 
 output "aks_kube_admin_config_cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.base.kube_admin_config.cluster_ca_certificate
+  value = azurerm_kubernetes_cluster.base.kube_admin_config.*.cluster_ca_certificate
 }
 
 output "aks_kube_admin_config_host" {
-  value = azurerm_kubernetes_cluster.base.kube_admin_config.host
+  value = azurerm_kubernetes_cluster.base.kube_admin_config.*.host
 }
 
 output "aks_kube_admin_config_username" {
-  value = azurerm_kubernetes_cluster.base.kube_admin_config.username
+  value = azurerm_kubernetes_cluster.base.kube_admin_config.*.username
 }
 
 output "aks_kube_admin_config_password" {
-  value = azurerm_kubernetes_cluster.base.kube_admin_config.password
+  value = azurerm_kubernetes_cluster.base.kube_admin_config.*.password
 }
