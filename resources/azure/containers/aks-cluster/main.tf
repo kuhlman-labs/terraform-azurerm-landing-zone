268494b43dbc3d5903384a5b9258265d25566055
Brett Kuhlman <brettkuhlman@Brett-Kuhlmans-MacBook-Pro.local>
2019-07-24 12:37:11 -0400

    refactor aks resource to use dyn blocks

--- resources/azure/containers/aks-cluster/main.tf
+++ resources/azure/containers/aks-cluster/main.tf
@@ -11,62 +11,89 @@ data "azurerm_resource_group" "base" {
 ################
 
 resource "azurerm_kubernetes_cluster" "base" {
-  name                = "${data.azurerm_resource_group.base.name}-${var.resource_prefix}"
-  location            = data.azurerm_resource_group.base.location
-  dns_prefix          = var.dns_prefix
-  resource_group_name = data.azurerm_resource_group.base.name
-  #api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
-  kubernetes_version = var.kubernetes_version
+  name                            = "${data.azurerm_resource_group.base.name}-${var.resource_prefix}"
+  location                        = data.azurerm_resource_group.base.location
+  dns_prefix                      = var.dns_prefix
+  resource_group_name             = data.azurerm_resource_group.base.name
+  api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
+  kubernetes_version              = var.kubernetes_version
 
-  agent_pool_profile {
-    name           = var.agent_name
-    count          = var.agent_count
-    vm_size        = var.agent_vm_size
-    os_type        = var.agent_os
-    vnet_subnet_id = var.subnet_id
-    type           = var.agent_type
-  }
-  linux_profile {
-    admin_username = var.admin_user_name
-
-    ssh_key {
-      key_data = file(var.public_ssh_key_path)
-    }
-  }
   service_principal {
     client_id     = var.client_id
     client_secret = var.client_secret
   }
-  network_profile {
-    network_plugin     = var.network_plugin
-    dns_service_ip     = var.dns_service_ip
-    docker_bridge_cidr = var.docker_bridge_cidr
-    service_cidr       = var.service_cidr
+
+  dynamic "agent_pool_profile" {
+    for_each = var.agent_pool_profile
+    content {
+      name            = agent_pool_profile.name
+      count           = agent_pool_profile.count
+      vm_size         = agent_pool_profile.vm_size
+      max_pods        = agent_pool_profile.max_pods
+      os_disk_size_gb = agent_pool_profile.os_disk_size_gb
+      os_type         = agent_pool_profile.os_type
+      type            = agent_pool_profile.type
+      vnet_subnet_id  = agent_pool_profile.vnet_subnet_id
+    }
   }
-  /*
+
+  dynamic "linux_profile" {
+    for_each = var.linux_profile
+    content {
+      admin_username = linux_profile.admin_user_name
+      ssh_key {
+        key_data = linux_profile.key_data
+      }
+    }
+  }
+
+  dynamic "network_profile" {
+    for_each = var.network_profile
+    content {
+      network_plugin     = network_profile.network_plugin
+      network_policy     = network_profile.network_policy
+      dns_service_ip     = network_profile.dns_service_ip
+      docker_bridge_cidr = network_profile.docker_bridge_cidr
+      pod_cidr           = network_profile.pod_cidr
+      service_cidr       = network_profile.service_cidr
+    }
+  }
   addon_profile {
-    http_application_routing {
-      enabled = var.http_application_routing_enabled
+    dynamic "http_application_routing" {
+      for_each = var.http_application_routing
+      content {
+        enabled = http_application_routing.enabled
+      }
     }
-    oms_agent {
-      enabled                    = var.oms_agent_enabled
-      log_analytics_workspace_id = var.log_analytics_workspace_id
+    dynamic "oms_agent" {
+      for_each = var.oms_agent
+      content {
+        enabled                    = oms_agent.enabled
+        log_analytics_workspace_id = oms_agent.workspace_id
+      }
     }
-    aci_connector_linux {
-      enabled     = var.aci_connector_linux_enabled
-      subnet_name = var.aci_subnet_name
+    dynamic "aci_connector_linux" {
+      for_each = var.aci_connector_linux
+      content {
+        enabled     = aci_connector_linux.enabled
+        subnet_name = aci_connector_linux.subnet_name
+      }
     }
   }
-  role_based_access_control {
-    azure_active_directory {
-      client_app_id     = var.client_app_id
-      server_app_id     = var.server_app_id
-      server_app_secret = var.server_app_secret
-      tenant_id         = var.tenant_id
+
+  dynamic "role_based_access_control" {
+    for_each = var.role_based_access_control
+    content {
+      azure_active_directory {
+        client_app_id     = role_based_access_control.client_app_id
+        server_app_id     = role_based_access_control.server_app_id
+        server_app_secret = role_based_access_control.server_app_secret
+        tenant_id         = role_based_access_control.tenant_id
+      }
+      enabled = role_based_access_control.enabled
     }
-    enabled = var.rbac_enabled
   }
-  */
+
   tags = var.tags
 }
 
--- resources/azure/containers/aks-cluster/variables.tf
+++ resources/azure/containers/aks-cluster/variables.tf
@@ -54,35 +54,6 @@ variable "server_app_id" {
 
 #aks
 
-variable "http_application_routing_enabled" {
-  description = "(Required) Is HTTP Application Routing Enabled? Changing this forces a new resource to be created."
-  type        = string
-  default     = "false"
-}
-
-variable "oms_agent_enabled" {
-  description = "(Required) Is the OMS Agent Enabled?"
-  type        = string
-  default     = "false"
-}
-
-variable "aci_connector_linux_enabled" {
-  description = "(Required) Is the virtual node addon enabled?"
-  type        = string
-  default     = "false"
-}
-
-variable "rbac_enabled" {
-  description = "(Required) Is Role Based Access Control Enabled? Changing this forces a new resource to be created."
-  type        = string
-  default     = "false"
-}
-
-variable "name" {
-  description = "The name of the Managed Kubernetes Cluster to create. Changing this forces a new resource to be created."
-  type        = string
-}
-
 variable "kubernetes_version" {
   description = "Version of Kubernetes specified when creating the AKS managed cluster. If not specified, the latest recommended version will be used at provisioning time (but won't auto-upgrade)."
   type        = string
@@ -95,90 +66,125 @@ variable "dns_prefix" {
   default     = "agents"
 }
 
-variable "admin_user_name" {
-  description = "The Admin Username for the Cluster. Changing this forces a new resource to be created."
-  type        = string
-}
-
-variable "public_ssh_key_path" {
-  description = "The Public SSH Key used to access the cluster. Changing this forces a new resource to be created."
-  type        = string
-}
-
-variable "aci_subnet_name" {
-  description = "The subnet name for the virtual nodes to run."
-  type        = string
-  default     = null
-}
-
-variable "subnet_id" {
-  description = "The ID of the Subnet where the Agents in the Pool should be provisioned. Changing this forces a new resource to be created."
-  type        = string
-}
-
-variable "log_analytics_workspace_id" {
-  description = "The ID of the Log Analytics Workspace which the OMS Agent should send data to."
-  type        = string
-  default     = null
-}
-
 variable "api_server_authorized_ip_ranges" {
   description = "The IP ranges to whitelist for incoming traffic to the masters"
   type        = list
   default     = null
 }
 
-variable "agent_count" {
-  description = "Number of Agents (VMs) in the Pool. Possible values must be in the range of 1 to 100 (inclusive). Defaults to 1."
-  type        = string
-  default     = "1"
+variable "agent_pool_profile" {
+  description = "https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#agent_pool_profile"
+  type        = list
+  default = [
+    #NOTE: When network_plugin is set to azure - the vnet_subnet_id field in the agent_pool_profile block must be set.  
+    {
+      name            = "default"
+      count           = "1"
+      vm_size         = "Standard_B2s"
+      maxpods         = null
+      os_disk_size_gb = null
+      os_type         = null
+      type            = null
+      vnet_subnet_id  = null
+    }
+  ]
 }
 
-variable "agent_vm_size" {
-  description = "The size of each VM in the Agent Pool (e.g. Standard_F1). Changing this forces a new resource to be created."
-  type        = string
-  default     = "Standard_B2s"
+variable "linux_profile" {
+  description = "https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#linux_profile"
+  type        = list
+  default     = []
+  /*
+  [
+    {
+      admin_username = "admin"
+      key_data = "${path.module}/id_rsa.pub"
+    }
+  ]
+  */
 }
 
-variable "dns_service_ip" {
-  description = "IP address within the Kubernetes service address range that will be used by cluster service discovery (kube-dns). This is required when network_plugin is set to azure. Changing this forces a new resource to be created."
-  type        = string
-  default     = null
+
+variable "network_profile" {
+  description = "https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#network_profile"
+  type        = list
+  default     = []
+  /*
+  #NOTE: When network_plugin is set to azure - the vnet_subnet_id field in the agent_pool_profile block must be set.
+  [
+    {
+      network_plugin = "kubenet"
+      #This field can only be set when network_plugin is set to azure
+      network_policy = null
+      #This is required when network_plugin is set to azure
+      dns_service_ip = null
+      #This is required when network_plugin is set to azure
+      docker_bridge_cidr = null
+      #This field can only be set when network_plugin is set to kubenet.
+      pod_cidr = "172.16.0.0/16"
+      #This is required when network_plugin is set to azure.
+      service_cidr = null
+    }
+  ]
+  */
 }
 
-variable "docker_bridge_cidr" {
-  description = "IP address (in CIDR notation) used as the Docker bridge IP address on nodes. This is required when network_plugin is set to azure. Changing this forces a new resource to be created."
-  type        = string
-  default     = null
+variable "http_application_routing" {
+  description = "https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#http_application_routing"
+  type        = list
+  default     = []
+  /*
+  [
+    {
+      enabled = "true"
+    }
+  ]
+  */
 }
 
-variable "service_cidr" {
-  description = "The Network Range used by the Kubernetes service. This is required when network_plugin is set to azure. Changing this forces a new resource to be created."
-  type        = string
-  default     = null
+variable "oms_agent" {
+  description = "https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#oms_agent"
+  type        = list
+  default     = []
+  /*
+  [
+    {
+      enabled = "true"
+      log_analytics_workspace_id = "log_analytics_workspace_id"
+    }
+  ]
+  */
 }
 
-variable "agent_type" {
-  description = "Type of the Agent Pool. Possible values are AvailabilitySet and VirtualMachineScaleSets. Changing this forces a new resource to be created. Defaults to AvailabilitySet."
-  type        = string
-  default     = null
+variable "aci_connector_linux" {
+  description = "https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#aci_connector_linux"
+  type        = list
+  default     = []
+  /*
+  [
+    {
+      enabled = "true"
+      subnet_name = "subnet_name"
+    }
+  ]
+  */
 }
 
-variable "agent_name" {
-  description = "(Required) Unique name of the Agent Pool Profile in the context of the Subscription and Resource Group. Changing this forces a new resource to be created."
-  type        = string
-  default     = "agentpool"
-}
-
-variable "agent_os" {
-  description = "(Optional) The Operating System used for the Agents. Possible values are Linux and Windows. Changing this forces a new resource to be created. Defaults to Linux."
-  type        = "string"
-  default     = "Linux"
-}
-
-variable "network_plugin" {
-  description = " (Required) Network plugin to use for networking. Currently supported values are azure and kubenet. Changing this forces a new resource to be created."
-  type        = "string"
+variable "role_based_access_control" {
+  description = "https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html#role_based_access_control"
+  type        = list
+  default     = []
+  /*
+  [
+    {
+      enabled = "true"
+      client_app_id     = "client_app_id"
+      server_app_id     = "server_app_id"
+      server_app_secret = "server_app_secret"
+      tenant_id         = "tenant_id"
+    }
+  ]
+  */
 }
 
 #tags
@@ -188,3 +194,5 @@ variable "tags" {
   type        = map
   default     = {}
 }
+
+
