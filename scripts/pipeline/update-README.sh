0a45dc34ed7f5ffb74ee9ac2e267924c056a9d44
Brett Kuhlman <brettkuhlman@Brett-Kuhlmans-MacBook-Pro.local>
2019-08-07 15:27:45 -0400

    updating scripts

--- scripts/pipeline/update-README.sh
+++ scripts/pipeline/update-README.sh
@@ -3,7 +3,7 @@ set -e
 
 modules="environments/* modules/* resources/*"
 
-for dir in $(find ${modules} -maxdepth 3 -type d); do
+for dir in $(find ${modules} -type d); do
   cd "$dir/";
   if [ -f main.tf ]; 
   then
@@ -12,11 +12,6 @@ for dir in $(find ${modules} -maxdepth 3 -type d); do
     echo | terraform-config-inspect "$dir"  > "$dir/"README.md
   else
   cd -;
-  echo skipping $dir, not a module directory
+  echo skipping $dir, because it does not contain a terraform module
   fi;
 done
-
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
