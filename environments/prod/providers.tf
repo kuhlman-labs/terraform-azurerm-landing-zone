4b048cf67397a9f51954f14e75a95cdc7b355518
brettkuhlman <brett.kuhlman@outlook.com>
2019-08-19 08:14:19 -0400

    removing versions.tf

--- environments/prod/providers.tf
+++ environments/prod/providers.tf
@@ -3,14 +3,14 @@
 ###
 
 terraform {
+  required_version = ">= 0.12"
   backend "azurerm" {
     container_name = "tfstate"
   }
 }
 
 provider "azurerm" {
-
-  #https://www.terraform.io/docs/providers/azurerm/auth/service_principal_client_secret.html
+  version         = "~> 1.32.0"
   subscription_id = var.subscription_id
   client_id       = var.app_id
   client_secret   = var.client_secret
@@ -18,7 +18,8 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  alias = "shared-services"
+  alias   = "shared-services"
+  version = "~> 1.32.0"
 
   # https://www.terraform.io/docs/providers/azurerm/auth/service_principal_client_secret.html
   subscription_id = var.shared_services_subscription_id
--- environments/test/providers.tf
+++ environments/test/providers.tf
@@ -3,13 +3,14 @@
 ###
 
 terraform {
+  required_version = ">= 0.12"
   backend "azurerm" {
     container_name = "tfstate"
   }
 }
 
 provider "azurerm" {
-
+  version         = "~> 1.32.0"
   subscription_id = var.subscription_id
   client_id       = var.app_id
   client_secret   = var.client_secret
