b5afac2930a1060873ce14f83be31d6a8080c4b4
Brett Kuhlman <brett.w.kuhlman@gmail.com>
2020-05-15 19:33:06 -0400

    modifying test environment

--- environments/test/outputs.tf
+++ environments/test/outputs.tf
@@ -1,19 +1,3 @@
 ###
 # environment outputs
 ###
-/*
-output "test_vnet_name" {
-  value       = module.network_hub.vnet_hub_name
-  description = "Generated name of the hub virtual network"
-}
-
-output "test_vnet_id" {
-  value       = module.network_hub.vnet_hub_id
-  description = "Resource id of the hub virtual network"
-}
-
-output "test_vnet_rg" {
-  value       = module.network_hub.vnet_hub_rg
-  description = "Generated name of the resource group for hub virtual network"
-}
-*/
--- environments/test/providers.tf
+++ environments/test/providers.tf
@@ -4,21 +4,11 @@
 
 terraform {
   required_version = ">= 0.12"
-  backend "azurerm" {
-    resource_group_name  = "terraform-rg"
-    storage_account_name = "tfstatekuhlmanlabs"
-    container_name       = "tfstate"
-    key                  = "test.tfstate"
-  }
 }
 
 provider "azurerm" {
   version = ">= 2.7.0"
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.app_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
 provider "helm" {
