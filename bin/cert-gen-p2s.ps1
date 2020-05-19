9ae0fea86d68de8e3fe4a3dac3b04a06f0578aa5
Brett Kuhlman <brkuhlma@microsoft.com>
2020-05-18 20:16:09 -0400

    adding key_vault_p2S_cert module first pass

--- bin/cert-gen-p2s.ps1
+++ bin/cert-gen-p2s.ps1
@@ -1,10 +1,17 @@
 $cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
--Subject "CN=AzureP2SRootCert" -KeyExportPolicy Exportable `
+-Subject "CN=${root_cert_name}" -KeyExportPolicy Exportable `
 -HashAlgorithm sha256 -KeyLength 2048 `
 -CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
 
 New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature `
--Subject "CN=AzureP2SChildCert" -KeyExportPolicy Exportable `
+-Subject "CN=${child_cert_name}" -KeyExportPolicy Exportable `
 -HashAlgorithm sha256 -KeyLength 2048 `
 -CertStoreLocation "Cert:\CurrentUser\My" `
 -Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+
+
+$rootCertificate = Get-ChildItem -Path cert:\CurrentUser\My\ | where { $_.subject -eq "CN=${root_cert_name}" } 
+$childCertificate = Get-ChildItem -Path cert:\CurrentUser\My\ | where { $_.subject -eq "CN=${child_cert_name}" } 
+
+Export-Certificate -Cert $rootCertificate -FilePath "${file_path}/${root_cert_name}.cer"
+Export-Certificate -Cert $childCertificate -FilePath "${file_path}/${child_cert_name}.cer"
