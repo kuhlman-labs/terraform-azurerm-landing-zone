84c481250ba6921c767a2018aa26262d0dd100ab
Brett Kuhlman <brkuhlma@microsoft.com>
2020-05-27 18:02:04 -0400

    modifying domain_controllers module

--- modules/azure/domain_controllers/main.tf
+++ modules/azure/domain_controllers/main.tf
@@ -11,6 +11,237 @@ module "resource_group" {
   environment  = var.environment
 }
 
+module "subnet" {
+  source               = "../../../resources/azurerm/network/subnet"
+  resource_group       = module.resource_group.name
+  region               = module.resource_group.location
+  virtual_network_name = var.virtual_network_name
+  name_prefixes        = ["snet-active-directory"]
+  address_prefixes     = var.address_prefixes
+  environment          = var.environment
+}
+
+module "network_security_group" {
+  source         = "../../../resources/azurerm/network/network_security_group"
+  resource_group = module.resource_group.name
+  environment    = var.environment
+  region         = module.resource_group.location
+  policy_name    = "addsallow"
+}
+
+module "network_security_rule" {
+  source                      = "../../../resources/azurerm/network/network_security_rule"
+  resource_group              = module.resource_group.name
+  region                      = module.resource_group.location
+  network_security_group_name = module.network_security_group.name
+  network_security_rules = [
+    {
+      name                       = "LDAP"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "*"
+      source_port_range          = "389"
+      destination_port_range     = "389"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "LDAP SSL"
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "636"
+      destination_port_range     = "636"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "LDAP GC"
+      priority                   = 120
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "3268"
+      destination_port_range     = "3268"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "LDAP GC SSL"
+      priority                   = 130
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "3269"
+      destination_port_range     = "3269"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "Kerberos"
+      priority                   = 140
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "*"
+      source_port_range          = "88"
+      destination_port_range     = "88"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "DNS"
+      priority                   = 150
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "*"
+      source_port_range          = "53"
+      destination_port_range     = "53"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "SMB,CIFS,SMB2, DFSN, LSARPC, NbtSS, NetLogonR, SamR, SrvSvc"
+      priority                   = 160
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "*"
+      source_port_range          = "445"
+      destination_port_range     = "445"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "SMTP"
+      priority                   = 170
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "25"
+      destination_port_range     = "25"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "RPC, EPM"
+      priority                   = 180
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "*"
+      source_port_range          = "135"
+      destination_port_range     = "135"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "RPC, DCOM, EPM, DRSUAPI, NetLogonR, SamR, FRS"
+      priority                   = 190
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "49152-65535"
+      destination_port_range     = "49152-65535"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "RPC, DFSR (SYSVOL)"
+      priority                   = 200
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "5722"
+      destination_port_range     = "5722"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "Windows Time"
+      priority                   = 210
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "UDP"
+      source_port_range          = "123"
+      destination_port_range     = "123"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "Kerberos change/set password"
+      priority                   = 220
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "*"
+      source_port_range          = "464"
+      destination_port_range     = "464"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "DCOM, RPC, EPM"
+      priority                   = 230
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "UDP"
+      source_port_range          = "49152-65535"
+      destination_port_range     = "49152-65535"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "DFSN, NetLogon, NetBIOS Datagram Service"
+      priority                   = 240
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "UDP"
+      source_port_range          = "138"
+      destination_port_range     = "138"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "SOAP"
+      priority                   = 250
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "9389"
+      destination_port_range     = "9389"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "NetLogon, NetBIOS Name Resolution"
+      priority                   = 260
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "UDP"
+      source_port_range          = "137"
+      destination_port_range     = "137"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    },
+    {
+      name                       = "DFSN, NetBIOS Session Service, NetLogon"
+      priority                   = 270
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "TCP"
+      source_port_range          = "139"
+      destination_port_range     = "139"
+      source_address_prefix      = "VirtualNetwork"
+      destination_address_prefix = "*"
+    }  
+  ]
+}
+
+module "subnet_network_security_group_association" {
+  source = "../../../resources/azurerm/network/subnet_network_security_group_association"
+  subnet_id = module.subnet.id
+  network_security_group_id = module.network_security_group.id
+}
+
 #nic
 
 module "network_interface" {
@@ -20,8 +251,8 @@ module "network_interface" {
   environment                   = var.environment
   name_prefix                   = "nic-wvm-dc"
   nic_count                     = 2
-  subnet_id                     = var.subnet_id
-  private_ip_address_allocation = "Dynamic"
+  subnet_id                     = module.subnet.id
+  private_ip_address_allocation = "Static"
 }
 
 #vm
@@ -45,4 +276,28 @@ module "virtual_machine" {
   source_image_reference_offer     = "WindowsServer"
   source_image_reference_sku       = "2019-Datacenter"
   source_image_reference_version   = "latest"
+}
+
+module "managed_disk_1" {
+  source                           = "../../../resources/azurerm/compute/managed_disk"
+  resource_group                   = module.resource_group.name
+  region                           = module.resource_group.location
+  environment                      = var.environment
+  name_prefix                      = "data-disk-${module.virtual_machine[0].name}"
+}
+
+module "managed_disk_2" {
+  source                           = "../../../resources/azurerm/compute/managed_disk"
+  resource_group                   = module.resource_group.name
+  region                           = module.resource_group.location
+  environment                      = var.environment
+  name_prefix                      = "data-disk-${module.virtual_machine[1].name}"
+}
+
+module "virtual_machine_data_disk_attachment" {
+  source                           = "../../../resources/azurerm/compute/virtual_machine_data_disk_attachment"
+  resource_group                   = module.resource_group.name
+  region                           = module.resource_group.location
+  environment                      = var.environment
+  name_prefix                      = "data-disk-${module.virtual_machine[1].name}"
 }
--- resources/azurerm/compute/windows_virtual_machine/outputs.tf
+++ resources/azurerm/compute/windows_virtual_machine/outputs.tf
@@ -6,3 +6,8 @@ output "id" {
   value       = azurerm_windows_virtual_machine.base[*].id
   description = "The ID of the Windows Virtual Machine."
 }
+
+output "name" {
+  value       = azurerm_windows_virtual_machine.base[*].name
+  description = "The Name of the Windows Virtual Machine."
+}
