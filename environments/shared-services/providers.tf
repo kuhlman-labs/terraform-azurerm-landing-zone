a7df6c04c358fb2a6338edeaacf1e1a350ff6044
Brett Kuhlman <brett.w.kuhlman@gmail.com>
2020-06-12 09:38:35 -0400

    reverting provider

--- environments/shared-services/providers.tf
+++ environments/shared-services/providers.tf
@@ -3,10 +3,16 @@
 ###
 
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-    }
+  required_version = ">= 0.12"
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stterraformfstate000"
+    container_name       = "tfstate"
+    key                  = "shared-services.tfstate"
   }
-  required_version = ">= 0.13"
+}
+
+provider "azurerm" {
+  version = ">= 2.0.0"
+  features {}
 }
--- environments/test/providers.tf
+++ environments/test/providers.tf
@@ -3,11 +3,10 @@
 ###
 
 terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 2.14.0"
-    }
-  }
-  required_version = ">= 0.13"
+  required_version = ">= 0.12"
+}
+
+provider "azurerm" {
+  version = ">= 2.0.0"
+  features {}
 }
