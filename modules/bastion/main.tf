57c05280eab60115b0fe6e271efbb1f42152de5a
Brett Kuhlman <brett.w.kuhlman@gmail.com>
2020-07-05 13:38:51 -0400

    modifying module bastion

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -29,5 +29,6 @@ module "bastion" {
   address_prefix_bastion = var.address_prefix_bastion
   environment            = var.environment
   virtual_network_name   = var.virtual_network_name
+  virtual_network_resource_group = var.virtual_network_resource_group
   public_ip_address_id   = module.public_ip.id
 }
