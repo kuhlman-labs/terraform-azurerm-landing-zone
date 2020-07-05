59371cac09a59168dfe030ad6cd1e1163b4b433a
Brett Kuhlman <brett.w.kuhlman@gmail.com>
2020-07-05 13:40:07 -0400

    fmt

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -23,12 +23,12 @@ module "public_ip" {
 }
 
 module "bastion" {
-  source                 = "../../resources/network/bastion_host"
-  resource_group         = module.resource_group.name
-  region                 = module.resource_group.location
-  address_prefix_bastion = var.address_prefix_bastion
-  environment            = var.environment
-  virtual_network_name   = var.virtual_network_name
+  source                         = "../../resources/network/bastion_host"
+  resource_group                 = module.resource_group.name
+  region                         = module.resource_group.location
+  address_prefix_bastion         = var.address_prefix_bastion
+  environment                    = var.environment
+  virtual_network_name           = var.virtual_network_name
   virtual_network_resource_group = var.virtual_network_resource_group
-  public_ip_address_id   = module.public_ip.id
+  public_ip_address_id           = module.public_ip.id
 }
